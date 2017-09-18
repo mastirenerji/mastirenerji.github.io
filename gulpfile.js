f0d9a14580441fefc0ebb447b1f60c6fe5f33d55
Tahsin Kocaman <lordtrai@yahoo.com>
2017-09-18 15:10:24 +0300

    modified gulp task for dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 const gulp = require("gulp"),
-  browserSync = require("browser-sync").create(),
-  sass = require("gulp-sass"),
-  htmlmin = require("gulp-htmlmin"),
-  imagemin = require("gulp-imagemin");
+      browserSync = require("browser-sync").create(),
+      sass = require("gulp-sass"),
+      htmlmin = require("gulp-htmlmin"),
+      imagemin = require("gulp-imagemin"),
+      deploy = require('gulp-gh-pages');
 
 // Compile Sass & Inject Into Browser
 gulp.task("sass", function() {
@@ -10,6 +11,7 @@ gulp.task("sass", function() {
     .src(["node_modules/bootstrap/scss/bootstrap.scss", "src/scss/*.scss"])
     .pipe(sass())
     .pipe(gulp.dest("src/css"))
+    .pipe(gulp.dest("dist/css"))
     .pipe(browserSync.stream());
 });
 
@@ -22,34 +24,37 @@ gulp.task("js", function() {
       "node_modules/popper.js/dist/umd/popper.min.js"
     ])
     .pipe(gulp.dest("src/js"))
+    .pipe(gulp.dest("dist/js"))
     .pipe(browserSync.stream());
 });
 
 // Watch Sass & Serve
 gulp.task("serve", ["sass"], function() {
   browserSync.init({
-    server: "./src"
+    server: "./dist"
   });
 
   gulp.watch(
     ["node_modules/bootstrap/scss/bootstrap.scss", "src/scss/*.scss"],
     ["sass"]
   );
-  gulp.watch("src/*.html").on("change", browserSync.reload);
+  gulp.watch("dist/*.html").on("change", browserSync.reload);
 });
 
 // Move Fonts to src/fonts
 gulp.task("fonts", function() {
   return gulp
     .src("node_modules/font-awesome/fonts/*")
-    .pipe(gulp.dest("src/fonts"));
+    .pipe(gulp.dest("src/fonts"))
+    .pipe(gulp.dest("dist/fonts"));
 });
 
 // Move Font Awesome CSS to src/css
 gulp.task("fa", function() {
   return gulp
     .src("node_modules/font-awesome/css/font-awesome.min.css")
-    .pipe(gulp.dest("src/css"));
+    .pipe(gulp.dest("src/css"))
+    .pipe(gulp.dest("dist/css"));
 });
 
 gulp.task("minify-html", function() {
@@ -63,5 +68,17 @@ gulp.task("minify-img", function() {
     .pipe(imagemin())
     .pipe(gulp.dest("dist/img"))
 });
+
+/**
+ * Push build to gh-pages
+ * To make it effective add "deploy" to default task
+ */
+gulp.task("deploy", function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
+
+// Build
+gulp.task('build', ['minify-html', 'minify-img', 'fa', 'fonts', 'js', 'sass']);
 
 gulp.task("default", ["js", "serve", "fa", "fonts", "minify-html", "minify-img"]);
